a5da5fac2c809ac12a4e83866649a34a698d3867
Phiphir <phileas.ri@gmail.com>
2026-02-25 11:33:26 +0100

    Cleaned up version of a previous file

--- Dataset_Load_Clean.R
+++ Dataset_Load_Clean.R
@@ -1,19 +1,30 @@
+#File for loading the dataset
 
-###Loading the dataset###
+#Get necessary files and set them to variables
+label_file = ""
+dataset_file = ""
 
-df_labels <- read.csv2("labels.csv", sep = ",")
+#Load Labels for the dataset
+df_labels <- read.csv2(label_file, sep = ",")
+
+#Get the labels
 row_list <- df_labels$X
+
+#Load dataset with row names set as labels
 df_data <- read.csv2("data.csv", sep = ",",row.names = row_list)
 df_data$X <- NULL
+
+#Add the types of tumor to the dataset to sort later
 df_final <- cbind(df_labels$Class,df_data)  
 names(df_final)[names(df_final) == "df_labels$Class"] <-"Tumor_type"
 
+
+#Create a dataset with only breast cancer related data
 df_BRCA <- df_final[df_final$Tumor_type == 'BRCA',]
 
-write.csv(df_BRCA,"BRCA_df.csv",
-          quote=FALSE,sep = " ",eol = "\r\n",col.names = names(df_BRCA)
-          )
+#Export dataframe as csv 
+output_name = "test"
+write.csv(df_BRCA,output_name)
 
-df_test <-read.csv2("BRCA_only.csv", sep = ",")
-df_test2 <-read.csv2("BRCA_only_2.csv", sep = ",",row.names = )
-write.
+#Import dataset to check if it corectly exported
+df_test <- read.csv2(output_name, sep = ",")
