d385aeb3094ec9ffb9b4564574670048d92e8eef
oh-tilia <116599789+oh-tilia@users.noreply.github.com>
2026-02-27 15:33:07 +0100

    First PCA implementation on test dataset

---
+++ Import_and_see_PCA.R
@@ -0,0 +1,94 @@
+#Loading libraries
+library(dplyr)
+
+#Dataset we will be using 
+dataset <- "BRCA_Gene_Reads_Healthy_Cropped.csv"  #path of the dataset
+ 
+#Loading a dataset from a txt file 
+#df_BRCA <- read.table(dataset)
+
+#Loading a dataset from a csv file into a dataframe called df_BRCA
+df_BRCA <- read.csv2(dataset)
+
+
+
+#check for missing value
+values <- colSums(is.na(df_BRCA))
+check_missing <- data.frame(values)
+
+sample_names <- row.names(check_missing)
+for(i in range(length(check_missing$values))){
+  print(check_missing$values[i])
+  if(check_missing$values[i] !=0){
+    print("Value missing in:",sample_names[i])
+  }else {print("There are no missing values")
+  }
+}
+
+#checks for duplicate data in the name of the gene
+anyDuplicated(df_BRCA$Name) 
+
+#checks for duplicate data in the description of the gene
+anyDuplicated(df_BRCA$Description)
+
+#Viewing the column names to check the attributes
+col_names <- colnames(df_BRCA)
+
+#Checking the dataset was correctly imported
+View(df_BRCA)
+
+#transform values from the name and description column from character to factor
+#this could help later on when visualizing data using certain methods such as PCA
+#columns "Name" and "Description" are specific to our test dataset but need to be adapted
+df_BRCA <- df_BRCA %>% mutate_at(c('Name','Description'), as.factor)
+
+#Check correct mutation of data types (change the name after the $ to the actual column names)
+class(df_BRCA$Name)
+class(df_BRCA$Description)
+
+#Loop to check the classes of all the column in the dataframe
+#for(names in colnames(df_BRCA)){print(class(df_BRCA[[names]]))}
+
+#==============================================================================#
+#                                    PCA                                       #
+#==============================================================================#
+
+#Loading libraries
+library(ggplot2)
+
+#Select only necessary variables (omit name variables)
+X <- df_BRCA[, 3:70]
+
+#Standardize data
+scaled_X <- scale(X)
+
+#Compute PCA
+pca_result <- prcomp(scaled_X, center = TRUE, scale. = TRUE)
+pca_result
+
+#Extract principal components for each observation
+pca_data <- as.data.frame(pca_result$x[, 1:2])
+pca_data
+
+#Add Description/Name descriptor for classification
+pca_data$Name <- df_BRCA$Description
+pca_data
+
+#Create PCA graph using ggplot2
+ggplot(pca_data, aes(x = PC1, y = PC2, color = Name)) +
+  geom_point(size = 3) +
+  labs(title = "Analyse en Composantes Principales (PCA) sur df_BRCA",
+       x = "Première Composante Principale (PC1)",
+       y = "Deuxième Composante Principale (PC2)") +
+  theme_minimal()
+
+rm(pca_data,pca_result,scaled_X,X)
+
+
+
+
+
+
+
+
+
