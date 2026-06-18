df6293ee84228a6f5c1d4ca839a55982ea1b8eff
Philéas <phileas.ri@gmail.com>
2026-06-18 11:55:54 +0200

    Updated Join_Data.r
    Random forest model able to differentiate between cancerous/healthy cells based on gene expression, need more testing data to properly conclude on efficiency

--- Week_3/Join_Data.R
+++ Week_3/Join_Data.R
@@ -1,10 +1,13 @@
+library(tidymodels)
 library(dplyr)
+library(matrixStats)
+library(vip)
 
 #---------------------------------PARAMETERS------------------------------------
 
 n_top_rows <- 2000   #numbers of transcripts to be used
 
-df_BRCA <- read.csv2("../Datasets/GSE266566_full.csv")
+df_BRCA <- read.csv2("GSE266566_COUNTS.csv")
 df_HD <-read.csv2("GSE71862_MCF7_MCF10A_RSEM_expectedcounts.csv")
 
 #--------------------------------CLEANING---------------------------------------
@@ -52,7 +55,7 @@ metadata <- data.frame(
   stringsAsFactors = FALSE
 )
 
-metadata$knockdown <- ifelse(grepl("MCF10A", metadata$cell_line), "Healthy", "Cancerous")
+metadata$state <- ifelse(grepl("MCF10A", metadata$cell_line), "Healthy", "Cancerous")
 
 #remove variables that aren't going to be reused
 rm(sample_names)
@@ -90,4 +93,140 @@ log_top   <- log_cpm[top_idx, ]   #FINAL DF TO BE USED
 #remove variables that aren't going to be reused
 rm(expr_mat, keep, lib_sizes, cpm_mat, vars, log_cpm, top_idx)
 
+#--------------------------------FULL DATA PCA----------------------------------
+#load librairies
+library(FactoMineR)
+library(factoextra)
+
+#run PCA prompt
+pca_result <- prcomp(t(log_top), center = TRUE, scale. = TRUE)
+
+#PCA results on the 8 first dimensions
+pca_data  <- as.data.frame(pca_result$x[, 1:8])
+pca_data  <- cbind(pca_data, metadata) 
+pca_var_explained <- round(100 * pca_result$sdev^2 / sum(pca_result$sdev^2), 1)
+
+#PCA plot on PC1 & PC2
+ggplot(pca_data, aes(x = PC1, y = PC2, color = cell_line, shape = state)) +
+  geom_point(size = 3, alpha = 0.85) +
+  labs(title = "PCA - cancerous vs healthy",
+       x = paste0("PC1 (", pca_var_explained[1], "%)"),
+       y = paste0("PC2 (", pca_var_explained[2], "%)")) +
+  theme_minimal()
+
+#PCA plot on PC3 & PC4
+ggplot(pca_data, aes(x = PC3, y = PC4, color = cell_line, shape = state)) +
+  geom_point(size = 3, alpha = 0.85) +
+  labs(title = "PCA - cancerous vs healthy",
+       x = paste0("PC3 (", pca_var_explained[3], "%)"),
+       y = paste0("PC4 (", pca_var_explained[4], "%)")) +
+  theme_minimal()
+
+#prompt for accessing eigenvalues
+# --> eigenvalue = percentage of explained variance by a dimension
+pca_eig_val <- get_eigenvalue(pca_result)
+
+#bar plot of the eigenvalues 
+fviz_eig(pca_result, addlabels = TRUE, ylim = c(0, 50))
+#at dimension 6, we have 88.4% of the variance explained == keep 6 dimensions for machine learning 
+
+
+# #prompt for accessing results
+# pca_var <- get_pca_var(pca_result)
+# 
+# #most contributing descriptors in PC1 (first dim)
+# fviz_contrib(pca_result, choice = 'var', axes = 1, top = 40)
+# 
+# #extract loadings from PC1 and PC2 and put them in dataframe
+# pca_loadings <- data.frame(
+#   Variable = rownames(pca_result$rotation),
+#   PC1 = pca_result$rotation[,1],
+#   PC2 = pca_result$rotation[,2]
+# )
+# 
+# #sort drivers from PC1 and PC2
+# pc1_loadings_sorted <- pca_loadings[order(abs(pca_loadings$PC1), decreasing = TRUE), ]
+# pc2_loadings_sorted <- pca_loadings[order(abs(pca_loadings$PC2), decreasing = TRUE), ]
+# 
+# #remove variables that are not useful for other methods
+# rm(pca_data, pca_var_explained, pca_eig_val, pca_var, pca_loadings, pc1_loadings_sorted, pc2_loadings_sorted)
+
+
+#-----------------------------PCA LOADINGS EXTRACT------------------------------
+# extract loadings 
+loadings <- pca_result$rotation  # genes × PCs
+
+# choose Principal Components to consider, we take the first 6 --> see eigenvalues
+pcs_to_use <- 1:6
+
+# compute importance score per gene
+gene_scores <- rowSums(abs(loadings[, pcs_to_use]))
+
+# select top genes
+top_gene_n <- min(500, length(gene_scores))
+top_genes <- names(sort(gene_scores, decreasing = TRUE))[1:top_gene_n]
+
+#create a dataframe with only the top genes from log_top
+#transpose df to have our genes as cols
+rf_df <- as.data.frame(t(log_top[top_genes, ]))
+
+rm(loadings, pcs_to_use, gene_scores, top_gene_n)
+
+#------------------ML: CLASSIFICATION USING RANDOM FOREST-----------------------
+#set seed for reproducible results
+set.seed(222)
+
+#add knockdown target col from metadata
+rf_df$state <- factor(metadata$state)
+rf_df$cell_line <- factor(metadata$cell_line)
+
+#-----------------Knockdown recognition model
+
+#split our df to train the model (75% train / 25% test)
+split  <- initial_split(rf_df, prop = 0.75, strata = state)
+train  <- training(split)
+test   <- testing(split)
+
+#running Random Forest using ranger engine and permutation importance
+# --> permutation importance  in the ranger model for random forests measures how much the model's prediction error
+#     increases when the values of a feature are shuffled
+#     == helps id which features are most important for the model's predictions
+
+rf_spec <- rand_forest(mode = "classification", trees = 700, mtry = floor(sqrt(ncol(train)-1))) %>%
+  set_engine("ranger", importance = "permutation")
+
+train[,c(361,362,374)] <- NULL
+test[,c(361,362,374)] <- NULL
+
+rf_recipe <- recipe(state ~ ., data = train) %>%
+  step_zv(all_predictors())
+
+#fitting the model --> reduces the risk of overfitting, averaging the predictions of multiple trees
+rf_workflow <- workflow() %>%
+  add_model(rf_spec) %>%
+  add_recipe(rf_recipe)
+
+rf_fit <- fit(rf_workflow, data = train)
+
+#---Plotting Random Forest results---
+
+#predictions
+preds <- predict(rf_fit, test) %>%
+  bind_cols(test)
+
+#confusion matrix --> plotted as a heatmap
+conf_mat(preds, truth = state, estimate = .pred_class) %>% autoplot(type = "heatmap")
+
+#model performance
+metrics(preds, truth = state, estimate = .pred_class)
+
+#variable importance from the fitted ranger model
+rf_model <- extract_fit_parsnip(rf_fit)$fit
+rf_importance <- vip(rf_model, num_features = 20)
+
+#plot of the variable importance
+rf_importance
+
+#remove variables that aren't going to be reused
+rm(split, train, test, rf_spec, rf_recipe, rf_workflow, rf_fit, preds, rf_model, rf_importance)
 
